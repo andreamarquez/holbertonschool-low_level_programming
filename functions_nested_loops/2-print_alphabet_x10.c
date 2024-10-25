a002682e307919053428a8627ff74210c0c9ea13
andreamarquez <andreamarquezg.95@gmail.com>
2024-10-25 20:51:14 +0200

    2-print alphabet x10

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints the alphabet in lower case 10 times
+ *
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+	int index;
+
+	for (index = 1; index <= 10; index++)
+	{
+		print_alphabet();
+	}
+}
+
