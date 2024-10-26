49319b77ed32580a1c402da539f6afc272fe8305
andreamarquez <andreamarquezg.95@gmail.com>
2024-10-26 17:30:58 +0200

    indent, FORMAT

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -6,61 +6,37 @@
  */
 void print_to_98(int startingNumber)
 {
-    int currentNumber;
+	int currentNumber;
 
-    if (startingNumber <= 98)
-    {
-        for (currentNumber = startingNumber; currentNumber <= 98; currentNumber++)
-        {
-            if (currentNumber < 0)
-            {
-                _putchar('-');
-                _putchar(-currentNumber + '0');
-            }
-            else if (currentNumber >= 10)
-            {
-                if (currentNumber >= 100)
-                {
-                    _putchar((currentNumber / 100) + '0');
-                    _putchar(((currentNumber / 10) % 10) + '0');
-                }
-                else
-                {
-                    _putchar((currentNumber / 10) + '0');
-                }
-                _putchar((currentNumber % 10) + '0');
-            }
-            else
-            {
-                _putchar(currentNumber + '0');
-            }
-            if (currentNumber != 98)
-            {
-                _putchar(',');
-                _putchar(' ');
-            }
-        }
-    }
-    else
-    {
-        for (currentNumber = startingNumber; currentNumber >= 98; currentNumber--)
-        {
-            if (currentNumber >= 100)
-            {
-                _putchar((currentNumber / 100) + '0');
-                _putchar(((currentNumber / 10) % 10) + '0');
-            }
-            else if (currentNumber >= 10)
-            {
-                _putchar((currentNumber / 10) + '0');
-            }
-            _putchar((currentNumber % 10) + '0');
-            if (currentNumber != 98)
-            {
-                _putchar(',');
-                _putchar(' ');
-            }
-        }
-    }
-    _putchar('\n');
+	/**
+	 * Loop from startingNumber to 98,
+	 * incrementing or decrementing based on the value of startingNumber
+	 */
+	for (
+		currentNumber = startingNumber;
+		currentNumber != 98;
+		currentNumber += (startingNumber < 98) ? 1 : -1
+		)
+	{
+		if (currentNumber < 0)
+		{
+			_putchar('-');
+			_putchar(-currentNumber + '0');
+		}
+		else if (currentNumber >= 100)
+		{
+			_putchar((currentNumber / 100) + '0');
+			_putchar(((currentNumber / 10) % 10) + '0');
+		}
+		else if (currentNumber >= 10)
+		{
+			_putchar((currentNumber / 10) + '0');
+		}
+		_putchar((currentNumber % 10) + '0');
+		_putchar(',');
+		_putchar(' ');
+	}
+	_putchar('9');
+	_putchar('8');
+	_putchar('\n');
 }
