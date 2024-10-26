fa1d38f2ea607194956518005cb131eeaaa48f51
andreamarquez <andreamarquezg.95@gmail.com>
2024-10-26 15:15:53 +0200

    Fix betty + compiler conflicts

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -11,18 +11,21 @@
  */
 int print_sign(int n)
 {
+	int result;
+
 	if (n > 0)
 	{
 		_putchar('+');
-		return (1);
+		result = 1;
 	} else if (n < 0)
 	{
 		_putchar('-');
-		return (-1);
-	} else if (n == 0)
+		result = -1;
+	} else
 	{
 		_putchar('0');
-		return (0);
+		result = 0;
 	}
+	return (result);
 }
 
