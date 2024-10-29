640108e6b92eafe19bea0528e6c4ce8c9cc0d616
andreamarquez <andreamarquezg.95@gmail.com>
2024-10-29 15:21:39 +0100

    created

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - print numbers 0 to 9 (followed by a new line)
+ *
+ * Return: 0.
+ */
+void print_numbers(void)
+{
+	int number;
+
+	for (number = 0; number <= 9; number++)
+	{
+		_putchar(number);
+	}
+	_putchar('\n');
+}
