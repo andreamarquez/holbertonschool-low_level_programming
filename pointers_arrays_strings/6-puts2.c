5d47e790d0bfa0cf71a98405aeb2d0ef0c070bee
andreamarquez <andreamarquezg.95@gmail.com>
2024-11-02 00:01:59 +0100

    adds checks

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -1,7 +1,9 @@
 #include "main.h"
 
+int isEven(int n);
+
 /**
- * puts2 - prints every other character of a string,
+ * puts2 - prints every other character of a string -> skipping one in between,
  * starting with the first character, followed by a new line.
  * @pstr: pointer that points to the string ("chain of characters").
  *
@@ -14,10 +16,29 @@ void puts2(char *pstr)
 
 	while (currentChar != '\0')
 	{
-		_putchar(currentChar);
+		/**
+		 * Checks if the number is even (0, 2, 4, etc)
+		*/
+		if (isEven(charCounter))
+			_putchar(currentChar);
+
 		charCounter++;
 		nextPointer = (pstr + charCounter);
 		currentChar = *nextPointer;
 	}
 	_putchar('\n');
+}
+
+/**
+ * isEven - Checks if a number is even (0, 2, 4, etc).
+ * @number: The number to check.
+ *
+ * Return: 1 if the number is even, 0 otherwise.
+ */
+int isEven(int number)
+{
+	if (number % 2 == 0)
+		return (1);
+	else
+		return (0);
 }
