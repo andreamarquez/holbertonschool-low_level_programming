ae2f2d5cafe9103f9600b554e2bf0f1fb3e68de3
andreamarquez <andreamarquezg.95@gmail.com>
2024-11-02 15:25:45 +0100

    8-print_array

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_array - prints "n" elements (numberOfElementsToPrint)
+ * of an array of integers (arr), followed by a new line.
+ *
+ *
+ * @arr: array to print
+ * @numberOfElementsToPrint: `pointer` to the string that will be used
+ *
+ */
+void print_array(int *arr, int numberOfElementsToPrint)
+{
+	int currentIndex;
+
+	/*
+	* just until we print the numberOfElementsToPrint or until
+	* the size of array is found (shouldn't try to print more
+	* elements id the array already ended).
+	*
+	*/
+	for (
+		currentIndex = 0;
+		(currentIndex < numberOfElementsToPrint);
+		currentIndex++
+		)
+	{
+		printf("%d ", arr[currentIndex]);
+	}
+
+	printf("\n");
+}
