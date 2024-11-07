057e16b8b7737a6985bf51833bca5ba77cd02d2b
andreamarquez <andreamarquezg.95@gmail.com>
2024-11-07 22:36:26 +0100

    print_diagsums

--- pointers_arrays_strings/7-print_chessboard.c
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -3,18 +3,18 @@
 
 /**
  * print_chessboard - Prints the chessboard.
- * @a: the 2 dimension array pointer
+ * @main_array: the 2 dimension array pointer
  */
-void print_chessboard(char (*a)[8])
+void print_chessboard(char (*main_array)[8])
 {
-	int main_counter;
+	int array_counter;
 	int sub_counter;
 
-	for (main_counter = 0; main_counter < 8; main_counter++)
+	for (array_counter = 0; array_counter < 8; array_counter++)
 	{
 		for (sub_counter = 0; sub_counter < 8; sub_counter++)
 		{
-			putchar(a[main_counter][sub_counter]);
+			putchar(main_array[array_counter][sub_counter]);
 		}
 		putchar('\n');
 	}
--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -2,11 +2,22 @@
 #include "main.h"
 
 /**
- *print_diagsums - 
- * @a:
+ *print_diagsums - prints the sum of the two diagonals of a square matrix of integers.
+ * @matrix:
  * @size:
  */
-void print_diagsums(int *a, int size);
+void print_diagsums(int *a, int size)
 {
+    int index;
+    int primary_diagonal_sum = 0;
+    int secondary_diagonal_sum = 0;
 
+    for (index = 0; index < size; index++) {
+		/* Sum for primary diagonal */
+		primary_diagonal_sum = primary_diagonal_sum + a[index + (index * size)];
+		/* Sum for secondary diagonal */
+		secondary_diagonal_sum = secondary_diagonal_sum + a[(size - 1 - index) + (index * size)];
+    }
+
+    printf("%d, %d\n", primary_diagonal_sum, secondary_diagonal_sum);
 }
