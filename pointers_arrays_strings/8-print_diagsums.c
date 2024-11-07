a957a62dad9e65073ce9a6cc36fb884c1ac8d066
andreamarquez <andreamarquezg.95@gmail.com>
2024-11-07 22:41:37 +0100

    adds edge case

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -2,22 +2,32 @@
 #include "main.h"
 
 /**
- *print_diagsums - prints the sum of the two diagonals of a square matrix of integers.
- * @matrix:
- * @size:
+ * print_diagsums - prints the sum of the two
+ * diagonals of a square matrix of integers.
+ * @a: the square matrix that has been flattened
+ * @size: the size of the matrix
  */
 void print_diagsums(int *a, int size)
 {
-    int index;
-    int primary_diagonal_sum = 0;
-    int secondary_diagonal_sum = 0;
+	int index;
+	int primary_diagonal_sum = 0;
+	int secondary_diagonal_sum = 0;
 
-    for (index = 0; index < size; index++) {
+	if (size <= 0)
+	{
+		printf("0, 0\n");
+		return;
+	}
+
+	for (index = 0; index < size; index++)
+	{
 		/* Sum for primary diagonal */
-		primary_diagonal_sum = primary_diagonal_sum + a[index + (index * size)];
+		primary_diagonal_sum =
+			primary_diagonal_sum + a[index + (index * size)];
 		/* Sum for secondary diagonal */
-		secondary_diagonal_sum = secondary_diagonal_sum + a[(size - 1 - index) + (index * size)];
-    }
+		secondary_diagonal_sum =
+			secondary_diagonal_sum + a[(size - 1 - index) + (index * size)];
+	}
 
-    printf("%d, %d\n", primary_diagonal_sum, secondary_diagonal_sum);
+	printf("%d, %d\n", primary_diagonal_sum, secondary_diagonal_sum);
 }
