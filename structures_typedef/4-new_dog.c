0f58f5d55c825358c34a9fe1d90a7845a523f04d
andreamarquez <andreamarquezg.95@gmail.com>
2024-11-17 22:17:49 +0100

    ew_dog

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,90 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "dog.h"
+
+char *_strcpy(char *destination, char *source);
+
+/**
+ * new_dog - creates a new dog.
+ * Store a copy of name and owner.
+ * @name: Name of the dog.
+ * @age: The age of the dog.
+ * @owner: The owner of the dog.
+ * Return: NULL if function fails.
+*/
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t doggy;
+	dog_t *pointer;
+
+	char *name_copy = "";
+	char *owner_copy = "";
+
+	if (name == NULL || owner == NULL)
+		return (NULL);
+
+	_strcpy(name_copy, name);
+	_strcpy(owner_copy, owner);
+
+	doggy.name = name_copy;
+	doggy.age = age;
+	doggy.owner = owner_copy;
+	pointer = malloc(sizeof(dog_t));
+	if (pointer == NULL)
+	{
+		free(name_copy);
+		free(owner_copy);
+		return (NULL);
+	}
+	pointer = &doggy;
+
+	return (pointer);
+}
+
+/**
+ * _strcpy - copies the string pointed to by src,
+ * including the terminating null byte (\0), to the
+ * buffer pointed to by @destination
+ *
+ *
+ * @destination: copy address destination (for the string to copy)
+ * @source: the address of the original string to copy
+ *
+ * Return: the pointer to destination
+ *
+ */
+char *_strcpy(char *destination, char *source)
+{
+	int sourceCharCounter = 0;
+	char currentSourceChar = *source;
+	char *nextSourceCharPointer;
+
+	/*
+	* we copy until we reach the null terminator '\0'
+	* but as we need to include it, we will add it after the while
+	*/
+	while (currentSourceChar != '\0')
+	{
+		/*
+		* sourceCharCounter begins on 0 so the first address
+		* of source is used on the 1st iteration of the while,
+		* then we start jumping address positions:
+		* 78 + 0, 78 + 1
+		*
+		*/
+		nextSourceCharPointer = (source + sourceCharCounter);
+		currentSourceChar = *nextSourceCharPointer;
+		*(destination + sourceCharCounter) = currentSourceChar;
+		sourceCharCounter++;
+	}
+
+	/*
+	* sourceCharCounter now lets us jump to the very end of
+	* the string we formed, pointed by destination,
+	* to add a null Terminator
+	*
+	*/
+	*(destination + sourceCharCounter) = '\0';
+
+	return (destination);
+}
