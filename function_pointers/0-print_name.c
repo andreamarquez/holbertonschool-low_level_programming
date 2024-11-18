dbc791bb9d9a012d2ea4e95dc312886cecc81ab6
andreamarquez <andreamarquezg.95@gmail.com>
2024-11-18 18:37:44 +0100

    print_name

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * print_name - prints a name.
+ * The print_name function will call the function
+ * pointed to by f, passing the name string to it.
+ * @name: A pointer to a character of a string that is
+ * the name of the person.
+ * @function: a pointer to a function that takes a char and
+ * returns a void. This function will be applied to the name.
+ *
+ * Returns: Void.
+ */
+void print_name(char *name, void (*function)(char *))
+{
+	/* Check if name or function pointer is NULL */
+	if (name == NULL || function == NULL)
+	return;
+
+	/* Call the function pointed to by function with name as the argument */
+	function(name);
+}
---
+++ function_pointers/function_pointers.h
@@ -0,0 +1,6 @@
+#ifndef DOG_FUNCTIONS
+#define DOG_FUNCTIONS
+
+void print_name(char *name, void (*f)(char *));
+
+#endif /* DOG_FUNCTIONS */
