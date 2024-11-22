912c5bc688d78b015ddf25293005afc9598b66a0
andreamarquez <andreamarquezg.95@gmail.com>
2024-11-22 18:47:48 +0100

    test

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,10 +1,8 @@
 #ifndef VARIADIC_FUNCTIONS
 #define VARIADIC_FUNCTIONS
 
-int sum_them_all(const unsigned int n, ...);
-
-
-
-#endif /* VARIADIC_FUNCTIONS */
+int sum_them_all(const unsigned int number, ...);
 
 int _putchar(char c);
+
+#endif /* VARIADIC_FUNCTIONS */
