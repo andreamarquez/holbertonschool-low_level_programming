a58c7d478b6938a9e383cbb3d7b22e67a7e3556c
andreamarquez <andreamarquezg.95@gmail.com>
2024-12-07 17:19:43 +0100

    test

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -2,8 +2,8 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <errno.h>
 
 /**
@@ -19,7 +19,10 @@ int main(int argc, char **argv)
 	char buffer[1024];
 
 	if (argc != 3)
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
 
 	fd_source = open(argv[1], O_RDONLY);
 	if (fd_source == -1)
@@ -30,7 +33,11 @@ int main(int argc, char **argv)
 
 	fd_dest = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (fd_dest == -1)
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		close(fd_source);
+		exit(99);
+	}
 
 	while ((bytes_read = read(fd_source, buffer, 1024)) > 0)
 	{
@@ -38,20 +45,33 @@ int main(int argc, char **argv)
 		if (bytes_written != bytes_read)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			close(fd_source);
+			close(fd_dest);
 			exit(99);
 		}
 	}
 	if (bytes_read == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		close(fd_source);
+		close(fd_dest);
 		exit(98);
 	}
+
 	close_source = close(fd_source);
 	if (close_source == -1)
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_source), exit(100);
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_source);
+		close(fd_dest);
+		exit(100);
+	}
+
 	close_dest = close(fd_dest);
 	if (close_dest == -1)
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_dest), exit(100);
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_dest);
+		exit(100);
+	}
 
 	return (0);
 }
