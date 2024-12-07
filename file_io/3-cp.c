a007b4dbd7d2c1d8199874cad26e2dd3fb1c8ab7
andreamarquez <andreamarquezg.95@gmail.com>
2024-12-07 11:21:27 +0100

    print_error_and_exit

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -10,10 +10,11 @@
  * print_error_and_exit - Prints an error message to stderr and exits.
  * @message: The error message to print.
  * @exit_code: The exit code to use.
+ * @file: The file name to include in the error message.
  */
-void print_error_and_exit(const char *message, int exit_code)
+void print_error_and_exit(const char *message, int exit_code, const char *file)
 {
-	dprintf(STDERR_FILENO, "%s\n", message);
+	dprintf(STDERR_FILENO, message, file);
 	exit(exit_code);
 }
 
@@ -30,16 +31,19 @@ int main(int argc, char *argv[])
 	char buffer[BUFFER_SIZE];
 
 	if (argc != 3)
-		print_error_and_exit("Usage: cp file_from file_to", 97);
+		print_error_and_exit("Usage: cp %s %s\n", 97, "");
+
 	fd_from = open(argv[1], O_RDONLY);
 	if (fd_from == -1)
-		print_error_and_exit("Error: Can't read from file", 98);
+		print_error_and_exit("Error: Can't read from file %s\n", 98, argv[1]);
+
 	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (fd_to == -1)
 	{
 		close(fd_from);
-		print_error_and_exit("Error: Can't write to file", 99);
+		print_error_and_exit("Error: Can't write to %s\n", 99, argv[2]);
 	}
+
 	while ((read_bytes = read(fd_from, buffer, BUFFER_SIZE)) > 0)
 	{
 		written_bytes = write(fd_to, buffer, read_bytes);
@@ -47,19 +51,20 @@ int main(int argc, char *argv[])
 		{
 			close(fd_from);
 			close(fd_to);
-			print_error_and_exit("Error: Can't write to file", 99);
+			print_error_and_exit("Error: Can't write to %s\n", 99, argv[2]);
 		}
 	}
 	if (read_bytes == -1)
 	{
 		close(fd_from);
 		close(fd_to);
-		print_error_and_exit("Error: Can't read from file", 98);
+		print_error_and_exit("Error: Can't read from file %s\n", 98, argv[1]);
 	}
+
 	if (close(fd_from) == -1)
-		print_error_and_exit("Error: Can't close fd", 100);
+		print_error_and_exit("Error: Can't close fd %d\n", 100, argv[1]);
 
 	if (close(fd_to) == -1)
-		print_error_and_exit("Error: Can't close fd", 100);
+		print_error_and_exit("Error: Can't close fd %d\n", 100, argv[2]);
 	return (0);
-}
+	}
